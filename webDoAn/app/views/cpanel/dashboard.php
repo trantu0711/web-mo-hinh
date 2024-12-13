5989f7a98d036702a7d6966abc2bce7062e5fab1
trantu0711 <dh52107408@student.stu.edu.vn>
2024-12-13 14:01:29 +0700

    khung do an

---
+++ webDoAn/app/views/cpanel/dashboard.php
@@ -0,0 +1,1 @@
+<p>Welcome to Wesite Mô Hình</p>
---
+++ webDoAn/app/views/cpanel/order/order.php
@@ -0,0 +1,38 @@
+<h3 style="text-align: center;">Liệt kê đơn hàng</h3>
+<?php
+  if(!empty($_GET['msg'])){
+    $msg = unserialize(urldecode($_GET['msg']));
+    foreach ($msg as $key => $value){
+      echo '<span style="color:blue;font-weight:bold">'.$value.'</span>';
+    }
+  }
+  ?> 
+<table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Mã đơn hàng</th>
+        <th>Ngày đặt</th>
+        <th>Tình trạng</th>
+        <th>Quản lý</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      $i = 0;
+      foreach($order as $key => $ord){
+        $i++;
+       ?>
+      <tr>
+        <td><?php echo $i ?></td>
+        <td><?php echo $ord['order_code'] ?></td>
+        <td><?php echo $ord['order_date'] ?></td>
+        <td><?php if($ord['order_status']==0){echo 'Đơn hàng mới';}else{echo 'Hàng đã được duyệt';} ?></td>
+        <td><a href="<?php echo BASE_URL ?>/order/order_details/<?php echo $ord['order_code'] ?>">Xem đơn hàng</a></td>
+      </tr>
+      <?php
+      } 
+      ?>
+      
+    </tbody>
+  </table>

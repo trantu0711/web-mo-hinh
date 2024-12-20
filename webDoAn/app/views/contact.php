c2e0619e5310b8789e126f8d944aa0faa6de18d4
nguyenthien200403 <nguyenngocthien200403@gmail.com>
2024-12-21 02:48:25 +0700

    fix loi trang contact

--- webDoAn/app/views/contact.php
+++ webDoAn/app/views/contact.php
@@ -10,18 +10,11 @@
                               <em><strong>3t phone</strong></em><br />
                               <em> 241/4242 Nguyễn Văn Luông, Phường 11, Quận 6, HCM<br />
                               Điện thoại : 0837 217 365<br />
-                              <!--   <strong>Điện thoại: <span style="color:#FF0000"></span></strong></em> -->
+                            
                            </p>
                         </li>
                      </ul>
-                     <!--   <div class="hiring-box">
-                        <strong class="title">Chào bạn!</strong>
-                        
-                        <p>Mọi thắc mắc bạn hãy gửi về mail của chúng tôi <strong>thietbivanphong@gmail.com</strong> chúng tôi sẽ giải đáp cho bạn.</p>
-                        
-                        <p><a href="." class="arrow-link"><i class="fa fa-long-arrow-left" aria-hidden="true"></i> Về trang chủ</a></p>
-                        
-                        </div> -->
+                   
                   </div>
                </div>
                <div class="contact_right">
